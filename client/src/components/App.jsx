ee11b142e4396d5dd0f8dbc45f8820fe80ff5ed1
Raeye Jordan <raeyejordaan@yahoo.com>
2021-10-15 16:46:51 -0700

    testing react-router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './home/Home.jsx';
+import UserProfile from './userProfile/UserProfile.jsx';
 
 const App = () => (
-  <h1>Best Eats</h1>
+  <Router>
+    <div>
+      {/* <Header /> */}
+      <div>
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+
+        </Switch>
+      </div>
+      {/* <Footer /> */}
+    </div>
+  </Router>
 );
 
 export default App;
