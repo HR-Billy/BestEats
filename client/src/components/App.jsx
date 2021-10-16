20ae20493b034eeed70ca2a32063b586da5473a5
alan_fong <alanfong@berkeley.edu>
2021-10-15 17:19:41 -0700

    Added navigation bars
    Need to add css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 const App = () => (
-  <h1>Best Eats</h1>
+  <div>
+    <Router>
+      <div>
+        <Link to="/">Home </Link>
+        <Link to="/meal-plan">Meals </Link>
+        <Link to="/farmers">Farmers </Link>
+        <Link to="/store">Store </Link>
+        <Link to="/healthy">Health </Link>
+        <Link to="/subscribe">Subscribe</Link>
+      </div>
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <h1>Home component goes here</h1>
+        )}
+      />
+      <Route
+        exact
+        path="/meal-plan"
+        render={() => (
+          <h1>Meal Plan component goes here</h1>
+        )}
+      />
+      <Route
+        exact
+        path="/farmers"
+        render={() => (
+          <h1>Meet the farmers component goes here</h1>
+        )}
+      />
+      <Route
+        exact
+        path="/store"
+        render={() => (
+          <h1>Grocery store component goes here</h1>
+        )}
+      />
+      <Route
+        exact
+        path="/healthy"
+        render={() => (
+          <h1>Health section component goes here</h1>
+        )}
+      />
+      <Route
+        exact
+        path="/subscribe"
+        render={() => (
+          <h1>Subscription component goes here</h1>
+        )}
+      />
+    </Router>
+  </div>
 );
 
 export default App;
