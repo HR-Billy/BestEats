04eca38629d50b9937a9d06f9bdec116ede6b598
sparangan <savanna.parangan@gmail.com>
2021-10-16 13:37:02 -0700

    Add Home

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Home from './home/Home.jsx';
 
 const App = () => (
-  <h1>Best Eats</h1>
+  <>
+    <h1>Best Eats</h1>
+    <Home />
+  </>
 );
 
 export default App;
