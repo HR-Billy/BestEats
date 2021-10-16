21009f65701eac34443d008dc22fbab7b4ce4c88
Raeye Jordan <raeyejordaan@yahoo.com>
2021-10-16 11:42:08 -0700

    Skeleton building

--- client/src/components/profile/UserProfile.jsx
+++ client/src/components/profile/UserProfile.jsx
@@ -1,29 +1,35 @@
+/* eslint-disable import/extensions */
 import React, { useState } from 'react';
 import { Context } from './Context.jsx';
 import ProfilePic from './features/ProfilePic.jsx';
 import NutritionFacts from './features/NutritionFacts.jsx';
 import UpdateStatus from './features/UpdateStatus.jsx';
-import Feed from './features/feed.jsx';
+import Feed from './features/Feed.jsx';
 import Subscribe from './features/Subscribe.jsx';
 import Orders from './features/Orders.jsx';
+import { Wrapper, Container } from './profileStyles.jsx';
 
 const UserProfile = () => {
   const [user, setUser] = useState([]);
 
   return (
     <Context.Provider value={{ user, setUser }}>
-      <div>
-        <ProfilePic />
-        <NutritionFacts />
-
-        <UpdateStatus />
-        <Feed />
-
-        <Subscribe />
-        <Orders />
-        <Orders />
-        <Orders />
-      </div>
+      <Wrapper>
+        <Container>
+          <ProfilePic />
+          <NutritionFacts />
+        </Container>
+        <Container>
+          <UpdateStatus />
+          <Feed />
+        </Container>
+        <Container>
+          <Subscribe />
+          <Orders />
+          <Orders />
+          <Orders />
+        </Container>
+      </Wrapper>
     </Context.Provider>
   );
 };
---
+++ client/src/components/profile/profileStyles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+/* -------------------------------- MAIN ------------------------------------ */
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  border: 3px solid red;
+`;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default styled;
