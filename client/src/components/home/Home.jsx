3c4bd5b6b6566f1d249dcccd0ecc813b32fe968d
sparangan <savanna.parangan@gmail.com>
2021-10-18 14:05:33 -0700

    Add questions

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Landing from './Landing.jsx';
 import Tiles from './Tiles.jsx';
+import Questions from './Questions.jsx';
 
 const Home = () => (
   <>
     <Landing />
     <Tiles />
+    <Questions />
   </>
 );
 
