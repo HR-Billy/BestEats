4bf21153c13ad073b2720897a471d7ac2f7e2929
Raeye Jordan <raeyejordaan@yahoo.com>
2021-10-20 14:04:26 -0700

    working query for all data needed for profile get request

--- database/models/profile/profileQueries.js
+++ database/models/profile/profileQueries.js
@@ -1,6 +1,7 @@
 module.exports = {
   selectData: `
   SELECT json_build_object(
+    'id', id,
     'username', username,
     'first_name', first_name,
     'photo', 'add a photo column',
@@ -8,7 +9,12 @@ module.exports = {
     'member_start_date', member_start_date,
     'subscription_start_date', subscription_start_date,
     'weekly_start_date', weekly_start_date,
-    'status', (SELECT jsonb_agg( json_build_object( 'id', id, 'emoji', emoji, 'text', text, 'created_at', created_at))
+    'status', (SELECT jsonb_agg( json_build_object(
+      'status_id', id,
+      'emoji', emoji,
+      'text', text,
+      'created_at', created_at
+      ))
       FROM status
       WHERE user_id = $1),
     'meals', (SELECT jsonb_agg( json_build_object(
@@ -23,6 +29,24 @@ module.exports = {
       ))
       FROM user_meals
       INNER JOIN meal ON meal.id = user_meals.meal_id
+      WHERE user_id = $1),
+    'products', (SELECT jsonb_agg( json_build_object(
+      'product_id', products.id,
+      'date_ordered', user_products.date_ordered,
+      'date_received', user_products.date_received,
+      'product_name', products.product_name,
+      'category', products.category,
+      'calories', products.calories,
+      'carbs', products.carbs,
+      'protein', products.protein,
+      'fats', products.fats,
+      'image', products.image,
+      'quantity', products.quantity,
+      'serving_size', products.serving_size
+      ))
+      AS products
+      FROM user_products
+      INNER JOIN products ON products.id = user_products.product_id
       WHERE user_id = $1)
   ) as results
   FROM users
