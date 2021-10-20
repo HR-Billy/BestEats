881f09a2bba504b73fceb5b171b625b81d337a71
Raeye Jordan <raeyejordaan@yahoo.com>
2021-10-20 12:33:13 -0700

    partial query user data for

--- database/models/profile/profileQueries.js
+++ database/models/profile/profileQueries.js
@@ -1,5 +1,18 @@
 module.exports = {
   selectData: `
-  SELECT * FROM status WHERE user_id = $1;
+  SELECT json_agg( json_build_object(
+    'username', username,
+    'first_name', first_name,
+    'photo', 'add a photo column',
+    'subscribed', subscribed,
+    'member_start_date', member_start_date,
+    'subscription_start_date', subscription_start_date,
+    'weekly_start_date', weekly_start_date,
+    'status', (SELECT jsonb_agg( json_build_object( 'id', id, 'emoji', emoji, 'text', text, 'created_at', created_at))
+    FROM status
+    WHERE user_id = $1)
+  )) as results
+  FROM users
+  WHERE id = $1;
   `,
 };
