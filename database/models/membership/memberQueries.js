461d80c25fc0c2f3ec0af2d8fd74c908d2e9647d
Katherine <chiakyu@gmail.com>
2021-10-21 23:18:48 -0700

    delete unused query

--- database/models/membership/memberQueries.js
+++ database/models/membership/memberQueries.js
@@ -10,66 +10,6 @@ module.exports = {
   SELECT subscribed FROM users WHERE auth_id = $1;
   `,
 
-  // updateSubData:
-  // `INSERT INTO users
-  //   VALUES (
-  //     auth_id,
-  //     first_name,
-  //     last_name,
-  //     address1,
-  //     address2,
-  //     city,
-  //     state,
-  //     postal_code,
-  //     country,
-  //     subscribed,
-  //     subscription_start_date,
-  //     weekly_start_date,
-  //     allow_meals,
-  //     subscription_id,
-  //     meals_per_week,
-  //     cardholder_name,
-  //     card_number,
-  //     card_exp_date,
-  //     billing_address1,
-  //     billing_address2,
-  //     billing_city,
-  //     billing_state,
-  //     billing_postal_code,
-  //     billing_country
-  //   ) = (
-  //   $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
-  //   $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)
-  //   ON CONFLICT (auth_id) DO UPDATE
-  //   SET (
-  //     first_name,
-  //     last_name,
-  //     address1,
-  //     address2,
-  //     city,
-  //     state,
-  //     postal_code,
-  //     country,
-  //     subscribed,
-  //     subscription_start_date,
-  //     weekly_start_date,
-  //     allow_meals,
-  //     subscription_id,
-  //     meals_per_week,
-  //     cardholder_name,
-  //     card_number,
-  //     card_exp_date,
-  //     billing_address1,
-  //     billing_address2,
-  //     billing_city,
-  //     billing_state,
-  //     billing_postal_code,
-  //     billing_country
-  //   ) = (
-  //     $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
-  //     $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)
-  //     WHERE auth_id = $1`,
-
   updateSubData:
     `UPDATE users
     SET (
