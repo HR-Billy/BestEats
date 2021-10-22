49ef27a542c21c3854c347a796141e0af32fca19
jleiandy <jleiandy@gmail.com>
2021-10-22 13:35:00 -0700

    Refactored login button to hold the current url in state, which is then passed to the auth0provider

--- client/src/components/auth/login-button.jsx
+++ client/src/components/auth/login-button.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useLocation } from 'react-router-dom';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
+
+  const location = useLocation();
+
   return (
     <button
       className="btn btn-primary btn-block"
-      onClick={() => loginWithRedirect()}
+      onClick={() => {
+        loginWithRedirect({ appState: { returnTo: location?.pathname } });
+      }}
     >
       Log In
     </button>
