7038d41dc8fa1e69978166b2d04f36cfe21e3398
Katherine <chiakyu@gmail.com>
2021-10-22 09:43:33 -0700

    deleted some additional comments

--- client/src/components/home/Landing.jsx
+++ client/src/components/home/Landing.jsx
@@ -4,11 +4,6 @@ import { Link } from 'react-router-dom';
 import ProductHeroLayout from './ProductHero.jsx';
 
 const backgroundImage = 'https://wallpapercave.com/wp/wp2020196.jpg'; // bright fresh fruit "rgba(0, 0, 0, 0.4)"
-// const backgroundImage = 'https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'; // light with center spacing
-// const backgroundImage = 'https://images.pexels.com/photos/952479/pexels-photo-952479.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'; // dark with cutting board
-// const backgroundImage = 'https://images.pexels.com/photos/890507/pexels-photo-890507.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'; // groceries with variety of food
-// const backgroundImage = 'https://images.pexels.com/photos/7689003/pexels-photo-7689003.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'; // blue with chili
-// const backgroundImage = 'https://images.pexels.com/photos/1435895/pexels-photo-1435895.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'; // dining table
 
 const Landing = () => (
   <ProductHeroLayout
