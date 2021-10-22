794c8906fd95e2e00d4cc7a91cf1762a2805de23
Katherine <chiakyu@gmail.com>
2021-10-21 23:16:53 -0700

    update subscribe route for auth_id

--- database/models/membership/memberQueries.js
+++ database/models/membership/memberQueries.js
@@ -10,68 +10,37 @@ module.exports = {
   SELECT subscribed FROM users WHERE auth_id = $1;
   `,
 
-  updateSubData:
-  `INSERT INTO users
-    VALUES (
-      auth_id,
-      first_name,
-      last_name,
-      address1,
-      address2,
-      city,
-      state,
-      postal_code,
-      country,
-      subscribed,
-      subscription_start_date,
-      weekly_start_date,
-      allow_meals,
-      subscription_id,
-      meals_per_week,
-      cardholder_name,
-      card_number,
-      card_exp_date,
-      billing_address1,
-      billing_address2,
-      billing_city,
-      billing_state,
-      billing_postal_code,
-      billing_country
-    ) = (
-    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
-    $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)
-    ON CONFLICT (auth_id) DO UPDATE
-    SET (
-      first_name,
-      last_name,
-      address1,
-      address2,
-      city,
-      state,
-      postal_code,
-      country,
-      subscribed,
-      subscription_start_date,
-      weekly_start_date,
-      allow_meals,
-      subscription_id,
-      meals_per_week,
-      cardholder_name,
-      card_number,
-      card_exp_date,
-      billing_address1,
-      billing_address2,
-      billing_city,
-      billing_state,
-      billing_postal_code,
-      billing_country
-    ) = (
-      $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
-      $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)
-      WHERE auth_id = $1`,
-
   // updateSubData:
-  //   UPDATE users
+  // `INSERT INTO users
+  //   VALUES (
+  //     auth_id,
+  //     first_name,
+  //     last_name,
+  //     address1,
+  //     address2,
+  //     city,
+  //     state,
+  //     postal_code,
+  //     country,
+  //     subscribed,
+  //     subscription_start_date,
+  //     weekly_start_date,
+  //     allow_meals,
+  //     subscription_id,
+  //     meals_per_week,
+  //     cardholder_name,
+  //     card_number,
+  //     card_exp_date,
+  //     billing_address1,
+  //     billing_address2,
+  //     billing_city,
+  //     billing_state,
+  //     billing_postal_code,
+  //     billing_country
+  //   ) = (
+  //   $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
+  //   $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)
+  //   ON CONFLICT (auth_id) DO UPDATE
   //   SET (
   //     first_name,
   //     last_name,
@@ -99,5 +68,36 @@ module.exports = {
   //   ) = (
   //     $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
   //     $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)
-  //   WHERE id = $1`,
+  //     WHERE auth_id = $1`,
+
+  updateSubData:
+    `UPDATE users
+    SET (
+      first_name,
+      last_name,
+      address1,
+      address2,
+      city,
+      state,
+      postal_code,
+      country,
+      subscribed,
+      subscription_start_date,
+      weekly_start_date,
+      allow_meals,
+      subscription_id,
+      meals_per_week,
+      cardholder_name,
+      card_number,
+      card_exp_date,
+      billing_address1,
+      billing_address2,
+      billing_city,
+      billing_state,
+      billing_postal_code,
+      billing_country
+    ) = (
+      $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
+      $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)
+    WHERE auth_id = $1`,
 };
