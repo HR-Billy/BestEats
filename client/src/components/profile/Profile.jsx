d5ceca8a4ee542677a8efa121f1f64fd218124af
Raeye Jordan <raeyejordaan@yahoo.com>
2021-10-23 15:04:59 -0700

    cleaned up profile code

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react';
 import { Container, Grid, Paper, Typography, Avatar } from '@mui/material';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Context } from '../../Context.jsx';
 import { ProfileContext } from './ProfileContext.jsx';
 import NutritionFacts from './features/NutritionFacts.jsx';
 import Status from './features/Status.jsx';
