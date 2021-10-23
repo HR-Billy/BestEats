0467066a5af235416438f889082ef4651867f8e6
Raeye Jordan <raeyejordaan@yahoo.com>
2021-10-23 10:25:55 -0700

    queries now reflect auth_id

--- database/models/profile/profileQueries.js
+++ database/models/profile/profileQueries.js
@@ -11,7 +11,7 @@ module.exports = {
       'createdAt', created_at
       ) ORDER BY created_at DESC)
       FROM status
-      WHERE user_id = $1),
+      WHERE auth_id = $1),
     'suggested', 'insert suggested meals query',
     'arriving', (SELECT json_agg( json_build_object(
     'mealId', meal.id,
@@ -20,7 +20,7 @@ module.exports = {
     ) ORDER BY date_received DESC)
   FROM user_meal
   INNER JOIN meal ON meal.id = user_meal.meal_id
-  WHERE user_id = $1 AND date_received IS NULL),
+  WHERE auth_id = $1 AND date_received IS NULL),
     'recent', (SELECT json_agg( json_build_object(
     'mealId', meal.id,
     'mealName', meal.meal_name,
@@ -28,7 +28,7 @@ module.exports = {
     ) ORDER BY date_ordered DESC)
       FROM user_meal
       INNER JOIN meal ON meal.id = user_meal.meal_id
-      WHERE user_id = $1 AND date_received IS NOT NULL),
+      WHERE auth_id = $1 AND date_received IS NOT NULL),
     'products', 'insert product query',
     'nutritionFacts', ((SELECT json_agg( json_build_object(
       'name', meal.meal_name,
@@ -38,14 +38,14 @@ module.exports = {
       ) ORDER BY date_ordered DESC)
       FROM user_meal
       INNER JOIN meal ON meal.id = user_meal.meal_id
-      WHERE user_id = $1)),
+      WHERE auth_id = $1)),
     'mealAverages', (WITH avg AS
       (SELECT (sum(meal.carbs)/count(meal.carbs)) AS carbs,
       (sum(meal.fat)/count(meal.fat))AS fat,
       (sum(meal.calories) / count(meal.calories)) AS calories
       FROM user_meal
       INNER JOIN meal ON meal.id = user_meal.meal_id
-      WHERE user_id = $1)
+      WHERE auth_id = $1)
         SELECT json_agg (json_build_object(
           'carbsAverage', carbs,
           'fatAverage', fat,
@@ -56,9 +56,7 @@ module.exports = {
   WHERE id = $1;
   `,
   status: `
-  INSERT INTO status(user_id, text, created_At)
+  INSERT INTO status(auth_id, text, created_At)
   VALUES ($1, $2, $3);
   `,
 };
-
-// change all user_id to auth_id after refactoring
